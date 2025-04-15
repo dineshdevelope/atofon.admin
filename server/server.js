d7e848e91006ffc566226ddf0619a2e11e8697bb
Dinesh S <dineshindevelope@gmail.com>
2025-04-16 02:08:06 +0530

    added vercel.json file in backend

--- server/server.js
+++ server/server.js
@@ -9,6 +9,13 @@ dotenv.config();
 const PORT = process.env.PORT || 3000;
 
 app.use(cors());
+app.use(
+  cors({
+    origin: "*",
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+  })
+);
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
